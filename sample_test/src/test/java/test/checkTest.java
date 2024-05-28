3ffab07e70d41b3c7b3201e1166b65a0a24a7a73
THULASI <TULASIM428@GMAIL.COM>
2024-05-28 16:38:04 +0530

    done

---
+++ sample_test/src/test/java/test/checkTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class checkTest{
+@Test
+public void CreateAccountTest() {
+	System.out.println("done---> Account creation");
+}
+@Test
+public void deleteAccountTest() {
+	System.out.println("===account deleted===");
+}
+}
